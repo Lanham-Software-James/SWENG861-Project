419891905eb247d0fb0f0392740ffa5bc55c8bd8
James Lanham <james.r.lanham2021@gmail.com>
2023-01-11 13:28:49 -0500

    Update routing for the Music Artist Search Component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MusicArtistSearchComponent } from './components/music-artist-search/music-artist-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: MusicArtistSearchComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
