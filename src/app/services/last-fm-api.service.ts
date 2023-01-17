78e1d1c9c68fdad67078210b5a218555fcbc83cd
James Lanham <james.r.lanham2021@gmail.com>
2023-01-17 16:45:50 -0500

    Implemented artist search API call.

--- src/app/services/last-fm-api.service.ts
+++ src/app/services/last-fm-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Artist } from '../interfaces/artist';
 import { Track } from '../interfaces/track';
 
 const API_KEY= "ab42aa8d968952e516fe82b051edbb48";
@@ -15,6 +16,10 @@ export class LastFmApiService {
 
   public trackSearch(track_name: string, artist: string, page: number = 0, limit: number = 30): Observable<TrackSearchResponse> {
     return this.http.get<TrackSearchResponse>(`${ environment.last_fm_api_root }?method=track.search&api_key=${ environment.last_fm_api_key }&track=${ track_name }&artist=${ artist }&page=${ page }&limit=${ limit }&format=json`);
+  }
+
+  public artistSearch(artist: string, page: number = 0, limit: number = 30): Observable<ArtistSearchResponse> {
+    return this.http.get<any>(`${ environment.last_fm_api_root }?method=artist.search&api_key=${ environment.last_fm_api_key }&artist=${ artist }&page=${ page }&limit=${ limit }&format=json`);
   }
 }
 
@@ -30,4 +35,18 @@ interface TrackSearchDetails {
 
 interface TrackMatches {
   track: Track[]
+}
+
+interface ArtistSearchResponse {
+  results: ArtistSearchDetails,
+}
+
+interface ArtistSearchDetails {
+  'opensearch:startIndex': number,
+  'opensearch:totalResults': number,
+  artistmatches: ArtistMatches
+}
+
+interface ArtistMatches {
+  track: Artist[]
 }
