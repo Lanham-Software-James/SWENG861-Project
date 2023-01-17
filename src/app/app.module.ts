44ab5f87470b7314b2da645f5615f5d5f4322d5f
James Lanham <james.r.lanham2021@gmail.com>
2023-01-17 16:16:37 -0500

    Adding required imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MusicArtistSearchComponent } from './components/music-artist-search/music-artist-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrackSearchFormComponent } from './components/track-search-form/track-search-form.component';
+import { TrackSearchResultsComponent } from './components/track-search-results/track-search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MusicArtistSearchComponent
+    MusicArtistSearchComponent,
+    TrackSearchFormComponent,
+    TrackSearchResultsComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +31,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     BrowserAnimationsModule,
 
+    ReactiveFormsModule,
+
     MatPaginatorModule,
+    MatTabsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [
     
