01b5e929a77fe66f49eb66a22885a06e4fd06691
James Lanham <james.r.lanham2021@gmail.com>
2023-01-23 13:29:02 -0500

    Fixing tests.

--- src/app/components/music-artist-search/music-artist-search.component.spec.ts
+++ src/app/components/music-artist-search/music-artist-search.component.spec.ts
@@ -8,7 +8,9 @@ import { MusicArtistSearchComponent } from './music-artist-search.component';
 describe('MusicArtistSearchComponent', () => {
   let component: MusicArtistSearchComponent;
   let fixture: ComponentFixture<MusicArtistSearchComponent>;
+  let compiled: HTMLElement;
   const expectedTabLabels = ['Search Track', 'Search Album', 'Search Artist'];
+  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,6 +22,7 @@ describe('MusicArtistSearchComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MusicArtistSearchComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
     fixture.detectChanges();
     
   });
@@ -29,54 +32,50 @@ describe('MusicArtistSearchComponent', () => {
   });
 
   it('should render mat-tab-group element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content section mat-tab-group')).toBeDefined();
   });
 
   it('should render mat-tab-group element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content section mat-tab-group')).toBeDefined();
   });
 
   it('should render correct mat-tab labels', () => {
-    fixture.whenRenderingDone().then(() => {
-      const tabLabels = document.querySelectorAll(
-        '#menu .mat-tab-label-content'
-      );
-      Array.from(tabLabels).forEach(element => {
-          let value: string = element.textContent !== null ? element.textContent : "";
-          expect(expectedTabLabels).toContain(value);        
-      });
-    });
+    const tabLabels = compiled.querySelectorAll('#menu .mat-tab-label-content');
+
+    let test: boolean = true;
+
+    tabLabels.forEach(element => {
+      let value: string = element.textContent !== null ? element.textContent : "";
+      
+      if(!expectedTabLabels.includes(value)){
+        test = false;
+      }
+    });    
+
+    expect(test).toBeTrue();
   });
 
   it('should render app-track-search-form element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('app-track-search-form')).toBeDefined();
   });
 
   it('should render app-track-search-results element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('app-track-search-form')).toBeDefined();
   });
 
   it('should render app-artist-search-form element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('app-track-search-form')).toBeDefined();
   });
 
   it('should render app-artist-search-results element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('app-track-search-form')).toBeDefined();
   });
 
   it('should render app-album-search-form element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('app-track-search-form')).toBeDefined();
   });
 
   it('should render app-album-search-results element', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('app-track-search-form')).toBeDefined();
   });
 
@@ -88,7 +87,7 @@ describe('MusicArtistSearchComponent', () => {
 
     component.searchTrack(testValue);
 
-    expect(component.track_searched == true && component.track_search_details == testValue);
+    expect(component.track_searched == true && component.track_search_details == testValue).toBeTrue();
   });
   
   it('should set artist_searched to true and should set artist_search_details to specified value', () => {
@@ -98,7 +97,7 @@ describe('MusicArtistSearchComponent', () => {
 
     component.searchArtist(testValue);
 
-    expect(component.artist_searched == true && component.artist_search_details == testValue);
+    expect(component.artist_searched == true && component.artist_search_details == testValue).toBeTrue();
   });
 
   it('should set album_searched to true and should set album_search_details to specified value', () => {
@@ -108,6 +107,6 @@ describe('MusicArtistSearchComponent', () => {
 
     component.searchAlbum(testValue);
 
-    expect(component.album_searched == true && component.album_search_details == testValue);
+    expect(component.album_searched == true && component.album_search_details == testValue).toBeTrue();
   });
 });
